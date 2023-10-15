c7accc2557fc59e6131109affa8b5310747365ac
hadassaBrad <hadassa26162@gmail.com>
2023-10-15 17:22:19 +0300

    Initial Code + Welcome functions

---
+++ dotNet5784_9442_3421/Stage0/Program3421.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Targil0
+{
+  partial  class program
+    { static void Main(string[] args)
+        {
+            Welcome3421();
+            Welcome9442();
+            Console.ReadKey();
+
+        }
+        static partial void Welcome9442();
+
+        private static void Welcome3421()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+       
+        }
+    }
+}
+
