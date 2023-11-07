bdd4ecf4852d99005611c5ff5fbcbb47a2553d6c
SaraTaharani <taharani1517@gmail.com>
2023-11-07 15:55:54 +0200

    finish Initialization

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -2,16 +2,13 @@
 //namespace Dal:
 using DalApi;
 using DO;
-using System.Runtime.InteropServices;
-using System.Xml.Linq;
-
 public static class Initialization
 {
     public const int MIN_ID = 200000000;
     public const int MAX_ID = 400000000;
-    private static IDependency? d_dalDependency;
-    private static IEngineer? e_dalEngineer;
-    private static ITask? t_dalTask;
+    private static IDependency? s_dalDependency;
+    private static IEngineer? s_dalEngineer;
+    private static ITask? s_dalTask;
     private static readonly Random s_rand = new();
 
 
@@ -28,20 +25,81 @@ public static class Initialization
         "Sarale taharani",
         "Hodaya Shteinhouse",
         "Hadassa Bradpiece",
-        "Noa Marciano"
+        "Noa Marciano",
+        "Yossi Cohen",
+        "Shifra Weiss",
+        "Dina Bradpiece",
+        "Tamar Levi"
         };
-
 
         foreach (var _name in engineerNames)
         {
             int _id;
             do
-                _id = s_rand.Next(MIN_ID, MAX_ID);
-            while (e_dalEngineer!.Read(_id) != null);
+                _id = s_rand.Next(MIN_ID, MAX_ID + 1);
+            while (s_dalEngineer!.Read(_id) != null);
             string _email = _name + _id + "@gmail.com";
-            Engineer newEng = new(_id, _name,_email, s_rand.Next(50, 400), , true);//enum איך ליבא את
-            e_dalEngineer!.Create(newEng);
+            int _cost = 0;
+            EngineerExperience _level = (EngineerExperience)s_rand.Next(0, Enum.GetValues<EngineerExperience>().Count());
+            switch (_level)
+            {
+                case EngineerExperience.expert: _cost = 400;
+                    break;
+                case EngineerExperience.Jr:_cost = 300;
+                    break;
+                case EngineerExperience.rookie:_cost = 200;
+                    break;
+                default:
+                    break;
+            }
+            Engineer newEng = new(_id, _name, _email, _cost, _level, true);
+            s_dalEngineer!.Create(newEng);
         }
     }
-  
+
+    private static void createTask()
+    {
+        string[] taskDescriptions =
+        {
+            "building permits",//היתרי בניה
+            "organization",//התארגנות
+            "measurement",//מדידה
+            "Fencing the site",//גידור האתר
+            "digging",//חפירה
+            "grounding",//ביסוס
+            "plumbing",//אינסטלציה
+            "electrical power",//חשמל
+            "construction",//בניה
+            "sealing",//איטום
+            "frames",//מסגרות
+            "Elevators",//מעליות                    
+            "plaster",//גבס
+            "color"//צבע
+        };
+        List<Engineer> EngineersList = s_dalEngineer!.ReadAll();
+        for (int i = 0; i < 10; i++)
+        {
+            Task newTask = new(0, taskDescriptions[i], null, false, null, null, null, null, null, "", "", EngineersList[i].Id, EngineersList[i].Level, true);
+            s_dalTask!.Create(newTask);
+        }
+    }
+
+    private static void createDependency()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Dependency newDependency = new(0, i + 1, i);
+            s_dalDependency!.Create(newDependency);
+        }
+    }
+
+    public static void DO()
+    {
+        s_dalEngineer = s_dalEngineer ?? throw new NullReferenceException("DAL can not be null!");
+        s_dalTask = s_dalTask ?? throw new NullReferenceException("DAL can not be null!");
+        s_dalDependency = s_dalDependency ?? throw new NullReferenceException("DAL can not be null!");
+        createEngineer();
+        createTask();
+        createDependency();
+    }
 }
