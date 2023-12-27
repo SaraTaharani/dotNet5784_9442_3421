c310c966b5e1e6f9c8d6209c54d7700de8e35952
SaraTaharani <taharani1517@gmail.com>
2023-12-27 18:35:49 +0200

    start workino on TaskImplemenation

--- BL/BlImplementation/TaskImplementation.cs
+++ BL/BlImplementation/TaskImplementation.cs
@@ -1,13 +1,10 @@
-﻿
-
-namespace BlImplementation;
+﻿namespace BlImplementation;
 using BlApi;
-
 using System.Collections.Generic;
 
 internal class TaskImplementation : ITask
 {
-    public int Create(Task item)
+    public int Create(BO.Task item)
     {
         throw new NotImplementedException();
     }
@@ -17,17 +14,37 @@ internal class TaskImplementation : ITask
         throw new NotImplementedException();
     }
 
-    public Task? Read(int id)
+    public BO.Task? Read(int id)
     {
-        throw new NotImplementedException();
+        DO.Task? doTask = _dal.Task.Read(id);
+        if (doTask == null)
+            throw new BO.BlDoesNotExistException($"Student with ID={id} does Not exist");
+        return new BO.Student()
+        {
+            Id = id,
+            Name = doStudent.Name,
+            Alias = doStudent.Alias,
+            IsActive = doStudent.IsActive,
+            BirthDate = doStudent.BirthDate,
+            RegistrationDate = doStudent.RegistrationDate,
+            CurrentYear = (BO.Year)(DateTime.Now.Year - doStudent.RegistrationDate.Year)
+        };
+
     }
 
-    public IEnumerable<Task> ReadAll()
+    public IEnumerable<BO.Task> ReadAll()
     {
-        throw new NotImplementedException();
+        return (from DO.Task doTask in _dal.Task.ReadAll()
+                select new BO.TaskInList
+                {
+                    Id = doTask.Id,
+                    Description = doTask.Description,
+                    Alias=doTask.Alias,
+                 //   Status =doTask מה מציבים פה איזה סטטוס מכל האינמים
+                });
     }
 
-    public void Update(Task item)
+    public void Update(BO.Task item)
     {
         throw new NotImplementedException();
     }
