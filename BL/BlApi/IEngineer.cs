67a3ca96b821ba844c777f65f0a856f2ad9d7877
hadassaBrad <hadassa26162@gmail.com>
2023-12-27 17:30:46 +0200

    IEngineer in BLapi

---
+++ BL/BlApi/IEngineer.cs
@@ -0,0 +1,26 @@
+﻿using DO;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlApi
+{// An interface for the engineer
+    public interface IEngineer
+    {
+        public IEnumerable<BO.Engineer> ReadAll();//get all engineers
+        public BO.Engineer? Raed(int id);//GET A SPECIFIX ENGINEER
+
+        public int Create(BO.Engineer item);//add new engineer
+       public void Update(BO.Engineer item);//update an engineer
+       public void Delete(int id);//delete an engineer
+
+    }
+
+
+
+}
+}
