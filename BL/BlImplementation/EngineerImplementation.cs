b479f8a7ba5dbb7c9d6f4c9a952e4fbe2df3de88
hadassaBrad <hadassa26162@gmail.com>
2024-01-21 15:28:09 +0200

    fixing bugs

--- BL/BlImplementation/EngineerImplementation.cs
+++ BL/BlImplementation/EngineerImplementation.cs
@@ -91,8 +91,7 @@ internal class EngineerImplementation : IEngineer
                 Email = doEngineer.Email,
                 Level = (BO.EngineerExperience)doEngineer.Level!,
                 Cost = (double)doEngineer.Cost!,
-                Task = new BO.TaskInEngineer()
-                { Id = task!.Id, Alias = task.Alias! },
+               Task = task != null ? new BO.TaskInEngineer() { Id = task.Id, Alias = task.Alias! } : null,
             };
         if (filter == null)
             return allEngineers!;
--- PL/Enum.cs
+++ PL/Enum.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System;
-
+using System.Collections;
 namespace PL;
 internal class EngineerExperiencesCollection : IEnumerable
 {
